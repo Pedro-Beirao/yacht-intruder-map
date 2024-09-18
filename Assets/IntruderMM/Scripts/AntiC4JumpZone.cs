812f55227f13c2c12996a843b0a57db593fa5b40
Pedro Beirao <pedrocbeirao@gmail.com>
2024-09-18 14:35:26 +0100

    Add map

---
+++ Assets/IntruderMM/Scripts/AntiC4JumpZone.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AntiC4JumpZone : MonoBehaviour
+{
+    [HideInInspector]
+    public bool isAFirstZone = false;
+    public List<AntiC4JumpZone> requiredFirstZone = new List<AntiC4JumpZone>();
+
+    protected void Awake()
+    {
+        if (requiredFirstZone != null)
+        {
+            // isAFirstZone = false;
+
+            foreach (var zone in requiredFirstZone)
+            {
+                zone.isAFirstZone = true;
+            }
+        }
+
+        if (TryGetComponent(out Collider coll))
+        {
+            coll.isTrigger = true;
+        }
+    }
+}
---
+++ Assets/SuperbossDoor/ReadMeAndLicense.txt
@@ -0,0 +1,11 @@
+Version 0.15
+
+Copyright Superboss Games, Inc. 2021
+
+Everything included in this package including 3d models, doors, textures, materials, etc. is to only be used for Superboss Games' Intruder and Intruder Map Maker.
+
+Please inform me of any issues on https://superbossgames.com/chat
+
+Future versions will include empty door handles and hinges so you can more easily attach your own meshes.
+
+-Rob Storm
